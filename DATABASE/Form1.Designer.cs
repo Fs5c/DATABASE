41dfd04c7b4d82c4c07d7aaada35083283626947
farah <farah@DESKTOP-E1QJ39G>
2023-01-05 20:09:01 +0300

    Add project files.

---
+++ DATABASE/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace DATABASE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtStudentNo = new System.Windows.Forms.TextBox();
+            this.txtStudentFirstName = new System.Windows.Forms.TextBox();
+            this.txtStudentLastName = new System.Windows.Forms.TextBox();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Location = new System.Drawing.Point(30, 50);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(65, 13);
+            this.lblStudent.TabIndex = 0;
+            this.lblStudent.Text = "Student No.";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(29, 107);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(58, 13);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(275, 55);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(57, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // txtStudentNo
+            // 
+            this.txtStudentNo.Location = new System.Drawing.Point(92, 50);
+            this.txtStudentNo.Name = "txtStudentNo";
+            this.txtStudentNo.Size = new System.Drawing.Size(153, 20);
+            this.txtStudentNo.TabIndex = 3;
+            // 
+            // txtStudentFirstName
+            // 
+            this.txtStudentFirstName.Location = new System.Drawing.Point(92, 100);
+            this.txtStudentFirstName.Name = "txtStudentFirstName";
+            this.txtStudentFirstName.Size = new System.Drawing.Size(153, 20);
+            this.txtStudentFirstName.TabIndex = 4;
+            // 
+            // txtStudentLastName
+            // 
+            this.txtStudentLastName.Location = new System.Drawing.Point(338, 50);
+            this.txtStudentLastName.Name = "txtStudentLastName";
+            this.txtStudentLastName.Size = new System.Drawing.Size(153, 20);
+            this.txtStudentLastName.TabIndex = 5;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(293, 107);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 6;
+            this.btnAddStudent.Text = "Add Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(549, 159);
+            this.dataGridView1.TabIndex = 7;
+          
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 305);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.txtStudentLastName);
+            this.Controls.Add(this.txtStudentFirstName);
+            this.Controls.Add(this.txtStudentNo);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtStudentNo;
+        private System.Windows.Forms.TextBox txtStudentFirstName;
+        private System.Windows.Forms.TextBox txtStudentLastName;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DATABASE/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DATABASE
+{
+    public partial class Form1 : Form
+    {
+        private string conncetionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Users\farah\\OneDrive\\المستندات\\ClothesData.accdb";
+        private OleDbConnection con;
+        public Form1()
+        {
+            con = new OleDbConnection(conncetionString);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+
+            InsertStudent(txtStudentNo.Text, txtStudentFirstName.Text, txtStudentLastName.Text);
+            ClearForm();
+            UpdateStudentsGired();
+            
+        }
+        private void UpdateStudentsGired()
+        { }
+        private void InsertStudent(string StudendNo, string StudentFirstName, string StudentLastName)
+        {
+            string insertCommand = $"Insert Into Student(StudentNo , StudentFirstName,StudentLastName) "
+                + $"values('{StudendNo}','{StudentFirstName}','{StudentLastName}')";
+            OleDbCommand command = new OleDbCommand(insertCommand, con);
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+     private void ClearForm()
+        {
+            txtStudentNo.Text = "";
+            txtStudentFirstName.Text="";
+            txtStudentLastName.Text=""; }
+
+    }
+}
